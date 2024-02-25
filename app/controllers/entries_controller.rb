2beaa2b36d343b8324501fdbd577f62a7a7fe13a
Anna Wickham <anna.wickham@kellogg.northwestern.edu>
2024-02-25 23:10:50 +0000

    homework 3 draft 4

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,20 +12,12 @@ class EntriesController < ApplicationController
 
   def create
     @entry = Entry.new
-
-    # assign user-entered form data to Contact's columns
     @contact["first_name"] = params["first_name"]
     @contact["last_name"] = params["last_name"]
     @contact["email"] = params["email"]
     @contact["phone_number"] = params["phone_number"]
-
-    # assign relationship between Contact and Company
     @contact["company_id"] = params["company_id"]
-
-    # save Contact row
     @contact.save
-
-    # redirect user
     redirect_to "/companies/#{@contact["company_id"]}"
   end
   
